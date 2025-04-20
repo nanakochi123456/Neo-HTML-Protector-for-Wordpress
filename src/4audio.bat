ad7b4ee787c6a7122cb8ab7ef8a8d1e1098d8a5f
nanoko <yy845jp@gmail.com>
2025-04-20 13:09:37 +0900

    ライセンス上グレーっぽいのでCBRに変更

--- src/4audio.bat
+++ src/4audio.bat
@@ -1,22 +1,22 @@
 : VBR2=96kbps by libfdk_aac
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/fastkakumei.wav -c:a libfdk_aac -vbr 2 audio/fastkakumei.m4a
+wsl ffmpeg -y -i audiosrc/fastkakumei.wav -c:a aac -b:a 96k audio/fastkakumei.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/fastkokken.wav -c:a libfdk_aac -vbr 2 audio/fastkokken.m4a
+wsl ffmpeg -y -i audiosrc/fastkokken.wav -c:a aac -b:a 96k audio/fastkokken.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/fasttokoroten.wav -c:a libfdk_aac -vbr 2 audio/fasttokoroten.m4a
+wsl ffmpeg -y -i audiosrc/fasttokoroten.wav -c:a aac -b:a 96k audio/fasttokoroten.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/msx_fanfa.wav -c:a libfdk_aac -vbr 2 audio/msx_fanfa.m4a
+wsl ffmpeg -y -i audiosrc/msx_fanfa.wav -c:a aac -b:a 96k audio/msx_fanfa.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/msx_open.wav -c:a libfdk_aac -vbr 2 audio/msx_open.m4a
+wsl ffmpeg -y -i audiosrc/msx_open.wav -c:a aac -b:a 96k audio/msx_open.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/sentou.wav -c:a libfdk_aac -vbr 2 audio/sentou.m4a
+wsl ffmpeg -y -i audiosrc/sentou.wav -c:a aac -b:a 96k audio/sentou.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/sm3_oyaji1.wav -c:a libfdk_aac -vbr 2 audio/sm3_oyaji1.m4a
+wsl ffmpeg -y -i audiosrc/sm3_oyaji1.wav -c:a aac -b:a 96k audio/sm3_oyaji1.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/sm3_oyaji2.wav -c:a libfdk_aac -vbr 2 audio/sm3_oyaji2.m4a
+wsl ffmpeg -y -i audiosrc/sm3_oyaji2.wav -c:a aac -b:a 96k audio/sm3_oyaji2.m4a
 
-wsl ~/ffmpeg_build/bin/ffmpeg -y -i audiosrc/w-fanfa.wav -c:a libfdk_aac -vbr 2 audio/w-fanfa.m4a
+wsl ffmpeg -y -i audiosrc/w-fanfa.wav -c:a aac -b:a 96k audio/w-fanfa.m4a
 
 exit
 
